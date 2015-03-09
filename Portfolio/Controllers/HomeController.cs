3659ebe1c8821f57bbd4b45b8bdb875e5249d280
Andrii Pelekh <pelekhav@gmail.com>
2015-03-08 23:44:25 -0600

    Changes 3-8-15

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -19,11 +19,44 @@ namespace Portfolio.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
+        [HttpGet]
+        public ActionResult ContactMe()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult ContactMe(Models.ContactForm contactForm)
+        {
+            try
+            {
+                //set the date created
+                contactForm.CreationDate = DateTime.Now;
+
+                Models.ContactFormEntities db = new Models.ContactFormEntities();
+                db.ContactForms.Add(contactForm);
+                db.SaveChanges();
+            }
+            catch (Exception exception)
+            {
+                ViewBag.Error = exception.Message;
+                return View("Error");
+            }
+
+            System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage("robot@apelekh.com", "andrii.pelekh@gmail.com");
+            message.Subject = "You got a message!";
+            message.Body = string.Format("{0} at {1} wrote you a message below: \n {2}", contactForm.Name, contactForm.EmailAddress, contactForm.TextMessage);
+            System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient("mail.dustinkraft.com");
+            client.Port = 587;
+            client.Credentials = new System.Net.NetworkCredential("postmaster@dustinkraft.com", "techIsFun1");
+
+            client.Send(message);
+
+            return RedirectToAction("ThankYou", "Home");
+        }
+
+        public ActionResult ThankYou()
+        {
             return View();
         }
     }
